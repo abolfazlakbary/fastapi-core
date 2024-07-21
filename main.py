22c2795e3b1a8bc246c33a9412cc94668ac4e0ce
akbari_a <akbari_a@cyberno.ir>
2024-07-21 15:54:21 +0330

    fixed issue with postgres asyncpg

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import asyncio
 
 
 if __name__ == "__main__":
-    asyncio.run(run_server())
-
-
-uvicorn.run(
-    app=app,
-    host=configs.app_host,
-    port=configs.app_port
-)
+    asyncio.run(
+        run_server(),
+        uvicorn.run(
+        app=app,
+        host=configs.app_host,
+        port=configs.app_port
+     )
+    )
